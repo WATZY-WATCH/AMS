5bcf82be3117aac368492c254e0537b035590e92
unknown <ckck5050@naver.com>
2020-02-15 23:18:09 +0900

    그룹 생성 기능 구현
    1. ams.group.domian 패키지 생성 (GroupVO 생성)
    2. ams.group.dao 패키지 생성(GroupDAO 생성, GroupDAOImpl 생성)
    3. ams.group.service 패키지 생성(GroupService 생성, GroupServiceImpl
       생성)
    4. ams.group.controller 패키지 생성(GroupController 생성)
    5. 그룹 생성 기능 구현 getCreate(), postCreate() 구현

---
+++ src/main/java/ams/group/dao/GroupDAOImpl.java
@@ -0,0 +1,20 @@
+package ams.group.dao;
+
+import javax.inject.Inject;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.stereotype.Repository;
+
+import ams.group.domain.GroupVO;
+
+@Repository
+public class GroupDAOImpl implements GroupDAO{
+	@Inject SqlSession sql;
+	
+	private static final String ns = "ams.group.mapper.groupMapper";
+	
+	@Override
+	public int createGroup(GroupVO vo) throws Exception {
+		return sql.insert(ns+".groupCreate", vo);
+	}
+}
