71d4640886c0fea53158cd3bd5bf50c0b28e1143
yuuil <yeyeah_1@daum.net>
2020-02-16 21:49:52 +0900

    클라이언트 키 삭제

--- src/main/java/ams/user/service/Kakaoapi.java
+++ src/main/java/ams/user/service/Kakaoapi.java
@@ -20,7 +20,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 
 @Service
 public class Kakaoapi {
-	private final static String CLIENT_ID = "718b94115712bb9ba0bde752892fae07";
+	private final static String CLIENT_ID = "";
 	private final static String REDIRECT_URI = "http://localhost:8080/oauth";
 	
 	public JsonNode getAccessToken(String authorize_code, String state) throws UnsupportedEncodingException {
