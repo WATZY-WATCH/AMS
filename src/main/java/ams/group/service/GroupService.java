2c2986ed0dd112ef345dc338d01610395c701511
unknown <ckck5050@naver.com>
2020-02-17 17:21:55 +0900

    그룹 찾기- 페이징 처리
    1. 해당 페이지와 페이지 당 보여줄 리스트 갯수를 위한 ams.group.domain.GroupCriteria 클래스생성
    2. 그룹의 총 갯수를 인자로한 페이징처리에 관한
    ams.group.domain.PageMaker 클래스 생성
    3. GroupContorller 매소드 추가
    4. GroupDAO, GroupDAOImpl, GroupService, GroupServiceImpl, GroupMapper에
    각각 메소드와 SQL문 추가.

--- src/main/java/ams/group/service/GroupService.java
+++ src/main/java/ams/group/service/GroupService.java
@@ -1,9 +1,15 @@
 package ams.group.service;
 
+import java.util.List;
+
+import ams.group.domain.GroupCriteria;
 import ams.group.domain.GroupMemberVO;
 import ams.group.domain.GroupVO;
 
 public interface GroupService {
 	public int createGroup(GroupVO vo) throws Exception;
 	public int createGroupMember(GroupMemberVO vo) throws Exception;
+	public List<GroupVO> listPage(int page)throws Exception;
+	public List<GroupVO> listCriteria(GroupCriteria cri)throws Exception;
+	public int countPaging(GroupCriteria cri)throws Exception;
 }
--- src/main/java/ams/group/service/GroupServiceImpl.java
+++ src/main/java/ams/group/service/GroupServiceImpl.java
@@ -1,10 +1,13 @@
 package ams.group.service;
 
+import java.util.List;
+
 import javax.inject.Inject;
 
 import org.springframework.stereotype.Service;
 
 import ams.group.dao.GroupDAO;
+import ams.group.domain.GroupCriteria;
 import ams.group.domain.GroupMemberVO;
 import ams.group.domain.GroupVO;
 
@@ -21,5 +24,17 @@ public class GroupServiceImpl implements GroupService {
 	@Override
 	public int createGroupMember(GroupMemberVO vo) throws Exception {
 		return dao.createGroupMember(vo);
+	}
+	@Override
+	public List<GroupVO> listPage(int page) throws Exception {
+		return dao.listPage(page);
+	}
+	@Override
+	public List<GroupVO> listCriteria(GroupCriteria cri) throws Exception {
+		return dao.listCriteria(cri);
+	}
+	@Override
+	public int countPaging(GroupCriteria cri) throws Exception {
+		return dao.countPaging(cri);
 	}
 }
